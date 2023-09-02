a820608beee17a1715d0ad3d5489c38db8f447e5
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2023-09-01 23:06:24 -0300

    [GS] Agregar notas de la clase

--- principal.py
+++ principal.py
@@ -1,5 +1,6 @@
 from ticket import *
 
+
 def principal():
     v_tickets = []
 
@@ -17,14 +18,19 @@ def principal():
 
         print('4. Buscar por patente y cabina (se muestra el primero encontrado).')
 
-        print('5. Buscar si existe en el arreglo un registro cuyo código de ticket sea igual a c, siendo c una valor que se carga por teclado. Si existe, cambiar el valor del campo forma de pago, de forma que pase a valer el valor contrario (si valía 1, que pase a valer 2, y viceversa), y luego mostrar el registro completo modificado. Si no existe indicar con un mensaje. La búsqueda debe detenerse al encontrar el primer registro que coincida con el criterio pedido.')
+        print(
+            '5. Buscar si existe en el arreglo un registro cuyo código de ticket sea igual a c, siendo c una valor que se carga por teclado. Si existe, cambiar el valor del campo forma de pago, de forma que pase a valer el valor contrario (si valía 1, que pase a valer 2, y viceversa), y luego mostrar el registro completo modificado. Si no existe indicar con un mensaje. La búsqueda debe detenerse al encontrar el primer registro que coincida con el criterio pedido.')
 
-        print('6. Determinar la cantidad de vehículos de cada país que pasaron por las cabinas, contando también los vehículos que no son de ninguno de los siete paises básicos.')
+        print(
+            '6. Determinar la cantidad de vehículos de cada país que pasaron por las cabinas, contando también los vehículos que no son de ninguno de los siete paises básicos.')
 
-        print('7. Determinar el importe acumulado por pagos de tickets, por cada uno de los posibles tipos de vehiculo (un acumulador por cada uno de los tres tipos).')
-        print('8. En base al resultado obtenido en el punto 7, determinar y mostrar cuál fue cuál fue el tipo de vehículo con mayor monto acumulado, e indicar además qué porcentaje representa ese monto mayor sobre el monto total.')
+        print(
+            '7. Determinar el importe acumulado por pagos de tickets, por cada uno de los posibles tipos de vehiculo (un acumulador por cada uno de los tres tipos).')
+        print(
+            '8. En base al resultado obtenido en el punto 7, determinar y mostrar cuál fue cuál fue el tipo de vehículo con mayor monto acumulado, e indicar además qué porcentaje representa ese monto mayor sobre el monto total.')
 
-        print('9. Calcular y mostrar la distancia promedio desde la última cabina recorrida entre todos los vehículos del arreglo, e informar además cuántos de los vehículos recorrieron una distancia mayor a ese promedio.')
+        print(
+            '9. Calcular y mostrar la distancia promedio desde la última cabina recorrida entre todos los vehículos del arreglo, e informar además cuántos de los vehículos recorrieron una distancia mayor a ese promedio.')
 
         print('10. Salir\n')
 
@@ -36,9 +42,10 @@ def principal():
             v_tickets = crear_arreglo(v_tickets)
         elif opc == 2:
             # Falta validación de cada campo
+            # ordenarlos?
             v_tickets = cargar_ticket(v_tickets)
         elif opc == 3:
-            # Falta ordenar de menor a mayor (id)
+            # Falta ordenar de menor a mayor (id) por binary search
             mostrar_registros(v_tickets)
         elif opc == 4:
             buscar_patente_cabina(v_tickets)
@@ -56,6 +63,7 @@ def principal():
     if not v_tickets == []:
         for t in v_tickets:
             print(t)
+
 
 if __name__ == "__main__":
     principal()
--- ticket.py
+++ ticket.py
@@ -1,4 +1,5 @@
 from platform import system
+
 """
 1) Código identificador del ticket (un número entero) [NUEVO].
 
@@ -18,9 +19,11 @@ from platform import system
 para indicar que la cabina actual es la primera que ese vehículo atraviesa).
 """
 
+
 class Ticket:
-    def __init__(self, id: int, patente: str, tipo_vehiculo: int, forma_pago: int, pais_cabina: int, distancia_km: int):
-        self.id = id
+    def __init__(self, registro_id: int, patente: str, tipo_vehiculo: int, forma_pago: int, pais_cabina: int,
+                 distancia_km: int):
+        self.registro_id = registro_id
         self.patente = patente
         self.tipo_vehiculo = tipo_vehiculo
         self.forma_pago = forma_pago
@@ -28,28 +31,33 @@ class Ticket:
         self.distancia_km = distancia_km
 
     def __str__(self) -> str:
-        return f"ID: {self.id}\nPatente: {self.patente}\nTipo de vehiculo: {self.tipo_vehiculo}\nForma de pago: {self.forma_pago}\nPais de la cabina: {self.pais_cabina}\nDistancia en km: {self.distancia_km}"
+        return f"ID: {self.registro_id}\nPatente: {self.patente}\nTipo de vehiculo: {self.tipo_vehiculo}" \
+               f"\nForma de pago: {self.forma_pago}\nPais de la cabina: {self.pais_cabina}\nDistancia en km: " \
+               f"{self.distancia_km}"
+
 
 # Punto 1
 def dividir_linea(linea):
     """
-    Esta funcion recorta el string de cada linea y devuelve cada tipo de dato.
+    Esta función recorta el string de cada línea y devuelve cada tipo de dato.
     :param linea: <str> registro de peaje
     :return: [patente, tipo_vehiculo, forma_de_pago, pais_cabina, distancia]
     """
 
-    id = linea[0:10]
-    patente = linea[10:17]
+    registro_id = linea[0:10]
+    # Eliminar espacios solo para las que tienen espacios
+    patente = linea[10:17].strip()
     tipo_vehiculo = linea[17]
     forma_pago = linea[18]
     pais_cabina = linea[19]
     distancia = linea[20:23]
 
-    return int(id), patente, int(tipo_vehiculo), int(forma_pago), int(pais_cabina), int(distancia)
+    return int(registro_id), patente, int(tipo_vehiculo), int(forma_pago), int(pais_cabina), int(distancia)
+
 
 def encontrar_idioma(primera_linea):
     """
-    Esta función determina el idioma en base a la primera línea de un registro de peaje.
+    Esta función determina el idioma según la primera línea de un registro de peaje.
     :param primera_linea: <str> primera línea del registro de peaje
     :return: <str> idioma encontrado "Portugués", "Español" o "Idioma no encontrado" si no se reconoce
     """
@@ -59,7 +67,8 @@ def encontrar_idioma(primera_linea):
     elif "ES" in primera_linea:
         return "Español"
     else:
-        return "Idioma no encontrado" 
+        return "Idioma no encontrado"
+
 
 def crear_arreglo(v_tickets):
     # Chequeo para sistemas Linux - Codificaciones de Windows no válidos para algunos distros de Linux
@@ -72,13 +81,13 @@ def crear_arreglo(v_tickets):
 
     archivo = open(ruta_archivo, encoding=codificacion)
     for linea in archivo:
-        if idioma == None: 
+        if idioma is None:
             idioma = encontrar_idioma(linea)
             continue
 
-        id, patente, tipo_vehiculo, forma_pago, pais_cabina, distancia_km = dividir_linea(linea)
+        registro_id, patente, tipo_vehiculo, forma_pago, pais_cabina, distancia_km = dividir_linea(linea)
 
-        ticket = Ticket(id, patente, tipo_vehiculo, forma_pago, pais_cabina, distancia_km)
+        ticket = Ticket(registro_id, patente, tipo_vehiculo, forma_pago, pais_cabina, distancia_km)
         v_tickets.append(ticket)
 
     archivo.close()
@@ -87,17 +96,22 @@ def crear_arreglo(v_tickets):
 
     return v_tickets
 
+
 # Punto 2
 def cargar_ticket(v_tickets):
     print("\nCargar ticket:")
 
     id = int(input("Ingrese el id (entero): "))
-    patente =  input("Ingrese la patente (string): ") 
-    tipo_vehiculo = int(input("Ingrese el tipo de vehículo (un entero entre 0 y 2 - (0: motocicleta, 1: automóvil, 2: camión)): "))
-    forma_pago = int(input("Ingrese la forma de pago (un dígito 1 o 2 que indica la forma de pago (1: manual, 2 telepeaje)): ")) 
-    pais_cabina = int(input("Ingrese el país de la cabina (un dígito entre 0 y 4 que indica el país donde está la cabina que hizo el cobro (0: Argentina - 1: Bolivia - 2: Brasil - 3: Paraguay - 4: Uruguay)): " )) 
-    distancia_km = int(input("Ingrese los kilómetros recorridos desde la cabina anterior (un entero que puede ser un cero para indicar que la cabina actual es la primera que ese vehículo atraviesa): " ))
-    
+    patente = input("Ingrese la patente (string): ")
+    tipo_vehiculo = int(
+        input("Ingrese el tipo de vehículo (un entero entre 0 y 2 - (0: motocicleta, 1: automóvil, 2: camión)): "))
+    forma_pago = int(
+        input("Ingrese la forma de pago (un dígito 1 o 2 que indica la forma de pago (1: manual, 2 telepeaje)): "))
+    pais_cabina = int(input(
+        "Ingrese el país de la cabina (0: Argentina - 1: Bolivia - 2: Brasil - 3: Paraguay - 4: Uruguay)): "))
+    distancia_km = int(input(
+        "Ingrese los kilómetros recorridos desde la cabina anterior (0 si es la primera): "))
+
     ticket = Ticket(id, patente, tipo_vehiculo, forma_pago, pais_cabina, distancia_km)
     v_tickets.append(ticket)
 
@@ -105,32 +119,46 @@ def cargar_ticket(v_tickets):
 
     return v_tickets
 
+
 # Punto 3
 def mostrar_registros(v_tickets):
-    if v_tickets == []:
-        print("No hay registros.")
+    if not v_tickets:
+        print("\nNo hay registros.")
     else:
         for t in v_tickets:
             print(" ")
             print(t)
 
+
 # Punto 4
 def buscar_patente_cabina(v_tickets):
-    if v_tickets == []:
+    if not v_tickets:
         print("\nNo hay registros.")
         return None
 
     print("\nBúsqueda:")
 
-    patente =  input("Ingrese la patente (string): ") 
-    pais_cabina = int(input("Ingrese el país de la cabina (un dígito entre 0 y 4 que indica el país donde está la cabina que hizo el cobro (0: Argentina - 1: Bolivia - 2: Brasil - 3: Paraguay - 4: Uruguay)): " )) 
-    
+    patente = input("Ingrese la patente (string): ")
+    pais_cabina = int(input(
+        "Ingrese el país de la cabina (0: Argentina - 1: Bolivia - 2: Brasil - 3: Paraguay - 4: Uruguay)): "))
+
     for t in v_tickets:
-        print(t.patente, type(t.patente), t.pais_cabina, type(t.pais_cabina))
-        print(t.patente == "KEYV04", t.pais_cabina == 4)
         if t.patente == patente and t.pais_cabina == pais_cabina:
-            print("Patente encontrada:")
+            print("\nPatente encontrada:")
             print(t)
             return None
 
     print("\n", " " * 30, "-" * 3, "Sin resultado disponible.", "-" * 3)
+
+
+def acumulado_por_cabina(v):
+    acumulador = [0] * 15
+    cant = [0] * 15
+
+    for vehiculo in v:
+        pos = vehiculo.cabina
+        acumulador[pos] += vehiculo.importe
+        cant[pos] += 1
+
+
+# Encontrar patente cargada por usuario
